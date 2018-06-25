508eadcdbc4f0f392da6eb1ccadba87dc0b17fba
anovsiradj <anov.siradj22@gmail.com>
2018-06-26 02:36:27 +0900

    update tests:1; rm original index.php file. make tests-1 simple-advanced (for example purpose).

--- tests/1.php
+++ tests/1.php
@@ -1,18 +1,16 @@
-<?php
-require 'reader.php';
+<?php // $> php tests/1.php
 
-$csv = new anovsiradj\csv\Reader('nabire-wanggar-wiraska.csv', array(
-	'line_delimiter' => ';',
-	'line_enclosure' => '"',
-));
+require __DIR__ . '/../vendor/autoload.php';
 
-$i = 0;
-foreach($csv->stream(10,10) as $buffer) {
-	$index = '[' . $i . ']';
+$csv = new anovsiradj\csv\Reader(__DIR__ . '/states.csv');
 
-	echo $index, implode(',', $buffer), PHP_EOL;
+/* HEADER - https://secure.php.net/manual/en/generator.current.php */
+$header = $csv->stream(0,1)->current();
+echo '[ ' , implode(' | ', $header) , ' ]', PHP_EOL;
 
-	$i++; if ($i >= 100) break;
+/* CONTENTS (first 10 entries) */
+foreach ($csv->stream(1,10) as $buffer) {
+	echo '[ ' , implode(' | ', $buffer) , ' ]', PHP_EOL;
 }
 
 $csv->close();
